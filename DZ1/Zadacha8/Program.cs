7be9b78bcf2791c5698521c484f50ded0e17c981
dima <alvisid92@gmail.com>
2022-12-16 10:38:17 +0300

    задача четыре

---
+++ DZ1/Zadacha8/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число N");
+int N = Convert.ToInt32(Console.ReadLine());
+int H = N;
+// while (H <= N)
+// {
+
+// }
+if (N % 2 == 0)
+{
+    Console.WriteLine($"{H}");
+    H = H + 1;
+}
