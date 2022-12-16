33d66cfb925ba4a6d32df7057efb6ccb39dd20a4
dima <alvisid92@gmail.com>
2022-12-16 10:05:44 +0300

    вторая задача

---
+++ DZ1/Zadacha4/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Ввести три числа для нахождения max:");
+int a , b , c;
+Console.WriteLine("Первое:");
+a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Второе:");
+b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Третье:");
+c = Convert.ToInt32(Console.ReadLine());
+
+
+int max = a;
+
+if (a > max) max = a;
+if ( b > max) max = b;
+if (c > max ) max = c;
+
+Console.Write("max = ");
+Console.WriteLine(max);
