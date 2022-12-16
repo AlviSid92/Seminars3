6d24397efa806c9033244b4c4de5df3d9555a3a7
dima <alvisid92@gmail.com>
2022-12-16 10:17:14 +0300

    третья задача

---
+++ DZ1/Zadacha6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число чтоб узнать четное или нет");
+int a;
+a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 2 == 0)
+{
+    Console.WriteLine("Четное");
+}
+else 
+{
+    Console.WriteLine("Нечетное");
+}
