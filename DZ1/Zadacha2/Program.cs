2bbe027f56965055c5d0474467d7b0352b6f97ad
dima <alvisid92@gmail.com>
2022-12-16 09:43:25 +0300

    первая задача

---
+++ DZ1/Zadacha2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Для сравнения введите два числа:");
+int a, b;
+Console.WriteLine("Число один:");
+a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Число два:");
+b = Convert.ToInt32(Console.ReadLine());
+bool C1 = a > b , C2 = a == b , C3 = a < b;
+
+if(C1)
+{
+    Console.WriteLine($"Number {a} > {b}");
+}
+else if(C2)
+{
+    Console.WriteLine($"Number {a} = {b}");
+}
+else
+{ 
+    Console.WriteLine($"Number {a} < {b}");
+}
