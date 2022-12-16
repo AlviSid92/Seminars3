83891fdd22dc69e0087948ff3d10031dffabe805
dima <alvisid92@gmail.com>
2022-12-16 11:47:23 +0300

    сохран

--- DZ1/Zadacha8/Program.cs
+++ DZ1/Zadacha8/Program.cs
@@ -1,12 +1,56 @@
-﻿Console.WriteLine("Введите число N");
-int N = Convert.ToInt32(Console.ReadLine());
-int H = N;
+﻿// Console.WriteLine("Введите число N");
+// int N = Convert.ToInt32(Console.ReadLine());
+// int H = N;
 // while (H <= N)
+// if (N % 2 == 0)
 // {
-
+//     Console.WriteLine($"{H}");
+//     H = H + 1;
 // }
-if (N % 2 == 0)
+// else
+// {
+//     H = H + 1;
+// }
+
+// Console.WriteLine("Ведите число");
+// int N = Convert.ToInt32(Console.ReadLine());
+// int Low = 0;
+
+// while( Low <= N )
+// {
+//     if (N % 2 == 0)
+// {
+//         Console.WriteLine($"{N}");
+// 	    Low = Low + 2;
+// }
+// //     else 
+// // {
+// //         Console.WriteLine($"{Fal}");
+// //         // Low = Low + 1;
+// // }
+// // Low = Low + 1;
+// }
+
+// Console.WriteLine("Число");
+// int N = Convert.ToInt32(Console.ReadLine());
+// int count = 1;
+
+// while (count <= N)
+// {
+// 	// if (N / 2 ==0)
+// 	// {
+// 	Console.WriteLine($"{count}");
+// 	count = count + 2;
+// 	}
+	
+// }
+
+Console.WriteLine("Число");
+int N = Convert.ToInt32(Console.ReadLine());
+int res = 1;
+
+while ( res <= N);
 {
-    Console.WriteLine($"{H}");
-    H = H + 1;
+	Console.WriteLine("res");
+	res = res + 1;
 }
